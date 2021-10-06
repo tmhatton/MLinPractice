4ab97f6d91a5267a187237deec7dc66eadb40ff1
Tom Hatton <46033252+tmhatton@users.noreply.github.com>
2021-10-06 15:19:47 +0200

    feat: Adds Precision, Recall and F1 score metrics for evaluation (#1)
    Allows the user to specify whether the trained model should be evaluated with the metrics Precision, Recall, and F1 score.

--- code/classification/run_classifier.py
+++ code/classification/run_classifier.py
@@ -10,16 +10,19 @@ Created on Wed Sep 29 14:23:48 2021
 
 import argparse, pickle
 from sklearn.dummy import DummyClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 # setting up CLI
-parser = argparse.ArgumentParser(description = "Classifier")
-parser.add_argument("input_file", help = "path to the input pickle file")
-parser.add_argument("-s", '--seed', type = int, help = "seed for the random number generator", default = None)
-parser.add_argument("-e", "--export_file", help = "export the trained classifier to the given location", default = None)
-parser.add_argument("-i", "--import_file", help = "import a trained classifier from the given location", default = None)
-parser.add_argument("-m", "--majority", action = "store_true", help = "majority class classifier")
-parser.add_argument("-a", "--accuracy", action = "store_true", help = "evaluate using accuracy")
+parser = argparse.ArgumentParser(description="Classifier")
+parser.add_argument("input_file", help="path to the input pickle file")
+parser.add_argument("-s", '--seed', type=int, help="seed for the random number generator", default=None)
+parser.add_argument("-e", "--export_file", help="export the trained classifier to the given location", default=None)
+parser.add_argument("-i", "--import_file", help="import a trained classifier from the given location", default=None)
+parser.add_argument("-m", "--majority", action="store_true", help="majority class classifier")
+parser.add_argument("-a", "--accuracy", action="store_true", help="evaluate using accuracy")
+parser.add_argument("-p", "--precision", action="store_true", help="evaluate using precision")
+parser.add_argument("-r", "--recall", action="store_true", help="evaluate using recall")
+parser.add_argument("-f1", "--f1_score", action="store_true", help="evaluate using f1")
 args = parser.parse_args()
 
 # load data
@@ -31,12 +34,12 @@ if args.import_file is not None:
     with open(args.import_file, 'rb') as f_in:
         classifier = pickle.load(f_in)
 
-else:   # manually set up a classifier
-    
+else:  # manually set up a classifier
+
     if args.majority:
         # majority vote classifier
         print("    majority vote classifier")
-        classifier = DummyClassifier(strategy = "most_frequent", random_state = args.seed)
+        classifier = DummyClassifier(strategy="most_frequent", random_state=args.seed)
         classifier.fit(data["features"], data["labels"])
 
 # now classify the given data
@@ -46,11 +49,17 @@ prediction = classifier.predict(data["features"])
 evaluation_metrics = []
 if args.accuracy:
     evaluation_metrics.append(("accuracy", accuracy_score))
+if args.precision:
+    evaluation_metrics.append(("precision", precision_score))
+if args.recall:
+    evaluation_metrics.append(("recall", recall_score))
+if args.f1_score:
+    evaluation_metrics.append(("f1_score", f1_score))
 
 # compute and print them
 for metric_name, metric in evaluation_metrics:
     print("    {0}: {1}".format(metric_name, metric(data["labels"], prediction)))
-    
+
 # export the trained classifier if the user wants us to do so
 if args.export_file is not None:
     with open(args.export_file, 'wb') as f_out:
