e3df0a672ac3c47592b57d3eefb8af86ae6a0774
Tom Hatton <tom-h@live.de>
2021-10-14 11:16:15 +0200

    Added unit test for URLsNum feature extractor

---
+++ test/feature_extraction/url_num_test.py
@@ -0,0 +1,26 @@
+import unittest
+import pandas as pd
+
+from code.feature_extraction.url_num import URLsNum
+from code.util import COLUMN_URLS
+
+
+class MentionNumTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.INPUT_COLUMN = COLUMN_URLS
+        self.extractor = URLsNum(self.INPUT_COLUMN)
+
+    def test_hashtag_num(self):
+        input = '''['www.google.com', 'www.apple.com', 'www.uos.de', 'www.example.com']'''
+        input_df = pd.DataFrame([COLUMN_URLS])
+        input_df[COLUMN_URLS] = [input]
+
+        expected_output = [4]
+        output = self.extractor.fit_transform(input_df)
+
+        self.assertEqual(expected_output, output)
+
+
+if __name__ == '__main__':
+    unittest.main()
