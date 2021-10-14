25b24051ec6c8da9d50a46d225dfa1159e981be8
Kai Dönnebrink <kai@doennebrink-lh.de>
2021-10-14 08:27:07 +0200

    Use correct pathes to data in preprocessing script (#7)
    We missed to fix some pathes in an earlier commit. Now the previous data directory is used again instead of code/data directory.

--- code/preprocessing.sh
+++ code/preprocessing.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # create directory if not yet existing
-mkdir -p code/data/preprocessing/split/
+mkdir -p data/preprocessing/split/
 
 #install all nltk models
 python -m nltk.downloader all
 
 # add labels
 echo "  creating labels"
-python -m code.preprocessing.create_labels code/data/raw/ code/data/preprocessing/labeled.csv
+python -m code.preprocessing.create_labels data/raw/ data/preprocessing/labeled.csv
 
 # other preprocessing (removing punctuation etc.)
 echo "  general preprocessing"
-python -m code.preprocessing.run_preprocessing code/data/preprocessing/labeled.csv code/data/preprocessing/preprocessed.csv --punctuation --tokenize -e code/data/preprocessing/pipeline.pickle
+python -m code.preprocessing.run_preprocessing data/preprocessing/labeled.csv data/preprocessing/preprocessed.csv --punctuation --tokenize -e code/data/preprocessing/pipeline.pickle
 
 # split the data set
 echo "  splitting the data set"
-python -m code.preprocessing.split_data code/data/preprocessing/preprocessed.csv code/data/preprocessing/split/ -s 42
+python -m code.preprocessing.split_data data/preprocessing/preprocessed.csv data/preprocessing/split/ -s 42
