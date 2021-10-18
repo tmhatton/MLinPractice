9c9771bc1d0618da270626ab76a2e963da283962
Mibalina <m.barkmann@outlook.de>
2021-10-18 12:01:34 +0200

    Added AUC-ROC metric
    - implemented in run_classifier.py
    - tested by running classification.sh
    - updated Readme.md

--- code/classification.sh
+++ code/classification.sh
@@ -5,7 +5,7 @@ mkdir -p data/classification/
 
 # run feature extraction on training set (may need to fit extractors)
 echo "  training set"
-python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle -nb --accuracy --precision
+python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle -nb --accuracy --precision -auc
 
 # run feature extraction on validation set (with pre-fit extractors)
 echo "  validation set"
