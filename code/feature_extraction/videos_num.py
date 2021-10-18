f790cf6c897e80e6ae6ff56ab3482e16bf0b4dc1
Mibalina <m.barkmann@outlook.de>
2021-10-18 09:21:23 +0200

    Add VideosNum feature
    - Write Unit test for the VideosNum feature extractor
    - Write VideosNum class
    - Add VideosNum feature extractor to `extract_features.py`
    - Add CLI arguments for VideosNum

---
+++ code/feature_extraction/videos_num.py
@@ -0,0 +1,33 @@
+"""
+Extracts the number of attached videos from the tweet
+"""
+import ast
+import numpy as np
+
+from code.feature_extraction.feature_extractor import FeatureExtractor
+from code.util import COLUMN_VIDEOS, SUFFIX_VIDEOS_NUM
+
+
+class VideosNum(FeatureExtractor):
+    """
+    Class to extracts the number of attached videos from the tweet
+    """
+
+    def __init__(self, input_column=COLUMN_VIDEOS):
+        super().__init__([input_column], COLUMN_VIDEOS + SUFFIX_VIDEOS_NUM)
+
+    # don't need to fit, so don't overwrite _set_variables()
+
+    # get the number of attached videos from the tweet
+    def _get_values(self, inputs):
+        nums_of_videos = []
+
+        for tweet in inputs[0]:
+            video_list = ast.literal_eval(tweet)
+            nums_of_videos.append(video_list)
+
+        nums_of_videos = np.array(nums_of_videos).reshape(-1, 1)
+
+        return nums_of_videos
+
+
