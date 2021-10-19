a5a3e7f0ce69f4eb27befd498786052aea8db126
Tom Hatton <tom-h@live.de>
2021-10-19 14:04:23 +0200

    Added gridsearch files and parameters for optimizing SVC

---
+++ code/classification/svm_grid_search.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+mkdir -p data/classification
+
+# specify hyperparameter values
+values_of_C=("0.00001 0.0001 0.001 0.01 0.1 0.0 10 100 1000 10000 100000")
+values_of_kernel=("linear poly rbf sigmoid precomputed")
+
+
+# different execution modes
+if [ "$1" = local ]
+then
+    echo "[local execution]"
+    cmd="code/classification/svm.sge"
+elif [ "$1" = grid ]
+then
+    echo "[grid execution"
+    cmd="qsub code/classification/svm.sge"
+else
+    echo "[ERROR! Argument not supported!]"
+    exit 1
+fi
+
+# do the grid search
+# shellcheck disable=SC2128
+
+for c in $values_of_C
+do
+echo "$c"
+for kernel in $values_of_kernel
+do
+    echo "$kernel"
+    $cmd 'data/classification/svm_'"$c"'_'"$kernel"'.pickle' -svm --svm_kernel "$kernel" --svm_C $c -s 7 --f1 --cohen --auc_roc
+done
+done
