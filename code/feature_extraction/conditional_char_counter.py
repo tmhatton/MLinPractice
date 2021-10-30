a2ab5b756b9c04557da217f98f0c96c6a054ef37
Kai Dönnebrink <kai@doennebrink-lh.de>
2021-10-30 18:33:27 +0200

    Refactor number of punctuation chars and number of capital letters feature extractors (#38)
    * Adds base class to count chars that fulfill a condition
    
    * Refactor feature number of punctuations
    
    * Refactor feature number of capital letters

---
+++ code/feature_extraction/conditional_char_counter.py
@@ -0,0 +1,60 @@
+"""
+Extracts features that are based on characters and a condition if the characters should be counted.
+"""
+import string
+import numpy as np
+
+from code.feature_extraction.feature_extractor import FeatureExtractor
+from code.util import SUFFIX_PUNC_NUM, SUFFIX_CAP_LETTERS
+
+
+class ConditionalCharCounter(FeatureExtractor):
+    """
+    Class to count the characters in a text that fulfill a certain condition.
+    """
+
+    def __init__(self, input_column, output_column):
+        super().__init__([input_column], output_column)
+
+    def _get_values(self, inputs):
+        """Counts the characters in a text that fulfill a certain condition."""
+        num_of_chars = []
+
+        for text in inputs[0]:
+            number = 0
+            for character in text:
+                if self._check_condition(character):
+                    number = number + 1
+            num_of_chars.append(number)
+
+        num_of_chars = np.array(num_of_chars).reshape(-1, 1)
+
+        return num_of_chars
+
+    def _check_condition(self, char: str):
+        """Checks if the condition for counting the character is met."""
+        return True
+
+
+class PunctuationNum(ConditionalCharCounter):
+    """
+    Class to count the number of punctuation characters in an input.
+    """
+
+    def __init__(self, input_column):
+        super().__init__(input_column, input_column + SUFFIX_PUNC_NUM)
+
+    def _check_condition(self, char: str):
+        return char in string.punctuation
+
+
+class CapLettersNum(ConditionalCharCounter):
+    """
+    Class to count the number of capital letters.
+    """
+
+    def __init__(self, input_column):
+        super().__init__(input_column, input_column + SUFFIX_CAP_LETTERS)
+
+    def _check_condition(self, char: str):
+        return char.isupper()
